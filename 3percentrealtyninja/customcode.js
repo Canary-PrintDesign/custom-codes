8973c83deed1011ebfe1940fc7a096ee1ac79785
adamish717 <adam@piechnik.ca>
2016-04-23 20:48:08 -0700

    Add 3 Percent Realty customs

---
+++ 3percentrealtyninja/customcode.js
@@ -0,0 +1,24 @@
+<script>
+var url = window.location.href;
+
+var temp = 'Requesting information in regards to: ' + window.location.href;
+$('#listing_contactform_fields').replaceWith('<div id="wufoo-zi7wps00p2epbj"></div>');
+</script>
+<script type="text/javascript">var zi7wps00p2epbj;(function(d, t) {
+var s = d.createElement(t), options = {
+'userName':'canaryprint',
+'formHash':'zi7wps00p2epbj',
+'autoResize':true,
+'height':'475',
+'async':true,
+'defaultValues':'field8=' + temp ,
+'host':'wufoo.com',
+'header':'show',
+'ssl':true};
+s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.com/scripts/embed/form.js';
+s.onload = s.onreadystatechange = function() {
+var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
+try { zi7wps00p2epbj = new WufooForm();zi7wps00p2epbj.initialize(options);zi7wps00p2epbj.display(); } catch (e) {}};
+var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
+})(document, 'script');
+</script>
